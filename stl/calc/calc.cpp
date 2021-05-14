3f2e98e5ab807bb4a6dcc2b0dd2d1140083644b0
happyRip <aborbzbk@gmail.com>
2021-05-14 22:16:22 +0200

    imroved calc help flag

--- stl/calc/calc.cpp
+++ stl/calc/calc.cpp
@@ -13,7 +13,7 @@ int main(int argc, char** argv)
     if (argc <= 1) {
         std::cout << "No expression given.\n";
         return -1;
-    } else if (std::string(argv[1]) == "help") {
+    } else if (std::string(argv[1]) == "-h" || std::string(argv[1]) == "--help") {
         std::cout << "Calculate an expression in Reverse Polish notation. Available operators:\n+ addition\n- subtraction\nx multiplication\n: division\n";
         return 0;
     }
@@ -48,6 +48,9 @@ int main(int argc, char** argv)
                     return -2;
             }
             numbers.push(result);
+        } else {
+            std::cout << "Incorrect expression.\n";
+            return -2;
         }
     }
     std::cout << numbers.top() << std::endl;
