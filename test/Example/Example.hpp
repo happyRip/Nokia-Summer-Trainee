653246de39726f9e20f2511ac6067bad911a001e
Aleksander Borowski <73070465+happyRip@users.noreply.github.com>
2021-05-24 18:18:44 +0200

    removed example

--- test/Example/Example.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef EXAMPLE_HPP
-#define EXAMPLE_HPP
-
-int MAC(int x, int y, int& sum);
-int square(int x);
-
-#endif /* EXAMPLE_HPP */
--- test/Example/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/aborb/Documents/GitHub/Nokia-Summer-Trainee/test/Example
-# Build directory: C:/Users/aborb/Documents/GitHub/Nokia-Summer-Trainee/test/Example/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googletest")
-subdirs("test")
