c5e39ac707b760605a08bfa668978caad01ad830
Aleksander Borowski <73070465+happyRip@users.noreply.github.com>
2021-05-24 22:17:19 +0200

    Delete test/RPN_calculator/build directory

--- test/RPN_calculator/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/aborb/Documents/GitHub/Nokia-Summer-Trainee/test/RPN_calculator
-# Build directory: C:/Users/aborb/Documents/GitHub/Nokia-Summer-Trainee/test/RPN_calculator/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googletest")
-subdirs("test")
